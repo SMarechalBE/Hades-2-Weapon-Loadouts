ad3846ba68715137611198dcf7dff4b75b16d90b
SMarechalBE <samuelmarechal94@gmail.com>
2026-01-26 00:05:38 +0100

    feat: add arcanas and keepsake current loadout in aspect page

--- src/reload.lua
+++ src/reload.lua
@@ -11,6 +11,31 @@ local function AssertGameState()
 	return true
 end
 
+local function ConcatStrToTable(str)
+	local t = {}
+	str:gsub("%a+", function(v)
+		table.insert(t, v)
+	end)
+
+	return t
+end
+
+local function EnableLoadoutBox(screen)
+	game.SetAlpha({
+		Id = screen.Components.LoadoutBox.Id,
+		Fraction = 1.0,
+		Duration = 0.0,
+	})
+end
+
+local function DisableLoadoutBox(screen)
+	game.SetAlpha({
+		Id = screen.Components.LoadoutBox.Id,
+		Fraction = 0.0,
+		Duration = 0.0,
+	})
+end
+
 --#region Weapon
 
 ---Get current Weapon
@@ -98,9 +123,25 @@ local function PrintPinnedBoons(boons)
 	boons = boons or ""
 	modutil.mod.Print("  Pinned Boons: " .. boons)
 end
+
+function PinnedBoonsStartFunction(screen, aspectName)
+	modutil.mod.Print("PinnedBoonsStartFunction: WIP")
+end
+function PinnedBoonsEndFunction(screen, aspectName)
+	modutil.mod.Print("PinnedBoonsEndFunction: WIP")
+end
+
 --#endregion Pinned Boons
 
 --#region Keepsakes
+local function GetAspectKeepsakes(aspectName)
+	if not config.Loadout[aspectName].Saved then
+		return {}
+	end
+
+	return ConcatStrToTable(config.Loadout[aspectName].Keepsakes)
+end
+
 local function GetKeepsakes()
 	local keepsakesLookup = startingKeepsakes.config and startingKeepsakes.config.keepsakes
 	if not keepsakesLookup then
@@ -142,6 +183,65 @@ local function PrintKeepsakes(keepsakes)
 	keepsakes = keepsakes or "Error"
 	modutil.mod.Print("  Keepsakes: " .. keepsakes)
 end
+
+local function CreateKeepsakeIcons(screen, aspectName)
+	screen.KeepsakeIconIds = {}
+
+	local traitSpacingX = 80
+	local traitSpacingY = 120
+	local traitStartX = 40
+	local traitStartY = 80
+
+	local iconScale = 0.5
+
+	local keepsakes = GetAspectKeepsakes(aspectName)
+
+	local yOffset = traitStartY
+	for r = 1, 5 do
+		local xOffset = traitStartX
+		for c = 1, 5 do
+			local traitName = keepsakes[((r - 1) * 5) + c]
+			local traitData = traitName and game.TraitData[traitName]
+			if not traitData then
+				return
+			end
+			local traitIcon = game.CreateScreenComponent({
+				Name = "TraitTrayIconButton",
+				X = xOffset,
+				Y = yOffset,
+				Group = screen.ComponentData.DefaultGroup,
+				Scale = iconScale,
+				Animation = game.GetTraitIcon(traitData),
+				Alpha = 1.0,
+			})
+
+			table.insert(screen.Components, traitIcon)
+			table.insert(screen.KeepsakeIconIds, traitIcon.Id)
+
+			xOffset = xOffset + traitSpacingX
+		end
+		yOffset = yOffset + traitSpacingY
+	end
+end
+local function DestroyKeepsakeIcons(screen)
+	if screen.KeepsakeIconIds and type(screen.KeepsakeIconIds) == "table" then
+		game.Destroy({ Ids = screen.KeepsakeIconIds })
+	end
+	screen.KeepsakeIconIds = {}
+end
+
+function KeepsakesStartFunction(screen, aspectName)
+	modutil.mod.Print("KeepsakesStartFunction: WIP")
+	EnableLoadoutBox(screen)
+	CreateKeepsakeIcons(screen, screen.ClipboardText)
+end
+
+function KeepsakesEndFunction(screen, aspectName)
+	modutil.mod.Print("KeepsakesEndFunction: WIP")
+	DisableLoadoutBox(screen)
+	DestroyKeepsakeIcons(screen)
+end
+
 --#endregion Keepsakes
 
 --#region Familiar
@@ -167,6 +267,15 @@ local function PrintFamiliar(familiarName)
 	familiarName = familiarName or "Error"
 	modutil.mod.Print("  Familiar: " .. familiarName)
 end
+
+function FamiliarsStartFunction(screen, aspectName)
+	modutil.mod.Print("FamiliarsStartFunction: WIP")
+end
+
+function FamiliarsEndFunction(screen, aspectName)
+	modutil.mod.Print("FamiliarsEndFunction: WIP")
+end
+
 --#endregion Familiar
 
 --#region Arcanas
@@ -185,6 +294,14 @@ local function GetArcanas()
 	return table.concat(arcanas, ",")
 end
 
+local function GetAspectArcanas(aspectName)
+	if not config.Loadout[aspectName].Saved then
+		return {}
+	end
+
+	return ConcatStrToTable(config.Loadout[aspectName].Arcanas)
+end
+
 local function LoadArcanas(arcanasStr)
 	if not AssertGameState() then
 		return
@@ -196,11 +313,7 @@ local function LoadArcanas(arcanasStr)
 	end
 
 	-- Equip specified arcanas (if unlocked)
-	local arcanas = {}
-	arcanasStr:gsub("%a+", function(arcana)
-		table.insert(arcanas, arcana)
-	end)
-
+	local arcanas = ArcanasStrToTable(arcanasStr)
 	for _, arcana in pairs(arcanas) do
 		local metaUpgradeState = game.GameState.MetaUpgradeState[arcana]
 		if metaUpgradeState and metaUpgradeState.Unlocked then
@@ -218,6 +331,107 @@ local function PrintArcanas(arcanas)
 	arcanas = arcanas or ""
 	modutil.mod.Print("  Arcanas: " .. arcanas)
 end
+
+local function DisplayArcanasIcons(screen)
+	if not screen then
+		modutil.mod.Print("Error: screen is nil, can't display arcana icons")
+		return
+	end
+
+	screen.ArcanaIconIds = {}
+
+	local traitSpacingX = 80
+	local traitSpacingY = 120
+	local traitStartX = 40
+	local traitStartY = 80
+
+	local iconScale = 0.18
+
+	local yOffset = traitStartY
+	for _, row in ipairs(game.MetaUpgradeDefaultCardLayout) do
+		local xOffset = traitStartX
+		for _, arcanaName in ipairs(row) do
+			local metaUpgradeCardData = game.MetaUpgradeCardData[arcanaName]
+
+			-- TODO: handle locked arcanas, grey them out
+			local traitIcon = game.CreateScreenComponent({
+				Name = "TraitTrayIconButton",
+				X = xOffset,
+				Y = yOffset,
+				Group = screen.ComponentData.DefaultGroup,
+				Animation = metaUpgradeCardData.Image,
+				Scale = iconScale,
+				Alpha = 1.0,
+			})
+
+			table.insert(screen.Components, traitIcon)
+			screen.ArcanaIconIds[arcanaName] = traitIcon.Id
+
+			xOffset = xOffset + traitSpacingX
+		end
+		yOffset = yOffset + traitSpacingY
+	end
+end
+
+local function EnableArcanaIcons(screen)
+	if game.TableLength(screen.ArcanaIconIds, true) == 0 then
+		DisplayArcanasIcons(screen)
+		return
+	end
+
+	for _, id in pairs(screen.ArcanaIconIds) do
+		game.SetAlpha({ Id = id, Fraction = 1.0, Duration = 0.0 })
+	end
+end
+
+local function DisableArcanaIcons(screen)
+	for _, id in pairs(screen.ArcanaIconIds) do
+		game.SetAlpha({ Id = id, Fraction = 0.0, Duration = 0.0 })
+	end
+end
+
+local function LinkArcanaFrames(screen, aspectName)
+	screen.ArcanaFrameIds = {}
+
+	local loadoutArcanas = GetAspectArcanas(aspectName)
+	for _, arcanaName in ipairs(loadoutArcanas) do
+		local traitIconId = screen.ArcanaIconIds and screen.ArcanaIconIds[arcanaName]
+		if traitIconId then
+			local traitFrameId = game.CreateScreenObstacle({
+				Name = "BlankObstacle",
+				Group = screen.ComponentData.DefaultGroup,
+				Scale = 0.6,
+				Alpha = 1.0,
+			})
+			game.SetAnimation({ Name = "DevCard_EquippedHighlight", DestinationId = traitFrameId })
+			game.Attach({ Id = traitFrameId, DestinationId = traitIconId })
+
+			table.insert(screen.ArcanaFrameIds, traitFrameId)
+		else
+			modutil.mod.Print("Error: can't get trait icon id for " .. arcanaName)
+		end
+	end
+end
+
+local function DestroyArcanaFrames(screen)
+	if screen.ArcanaFrameIds and type(screen.ArcanaFrameIds) == "table" then
+		game.Destroy({ Ids = screen.ArcanaFrameIds })
+	end
+	screen.ArcanaFrameIds = {}
+end
+
+function ArcanasStartFunction(screen, aspectName)
+	EnableLoadoutBox(screen)
+	EnableArcanaIcons(screen)
+	LinkArcanaFrames(screen, screen.ClipboardText)
+end
+
+function ArcanasEndFunction(screen, aspectName)
+	DisableLoadoutBox(screen)
+	DisableArcanaIcons(screen)
+	DestroyArcanaFrames(screen)
+end
+
 --#endregion Arcanas
 
 --#region Fear Oaths
@@ -284,4 +498,20 @@ function RestoreLoadout(aspect)
 	LoadFamiliar(loadout.Familiar)
 	LoadArcanas(loadout.Arcanas)
 	LoadFearOaths(loadout.FearOaths)
+end
+
+function DefaultModelStartFunction(screen, aspectName)
+	game.SetAlpha({
+		Id = screen.Components.WeaponImage.Id,
+		Fraction = 1.0,
+		Duration = 0.0,
+	})
+end
+
+function DefaultModelEndFunction(screen, aspectName)
+	game.SetAlpha({
+		Id = screen.Components.WeaponImage.Id,
+		Fraction = 0.0,
+		Duration = 0.0,
+	})
 end
